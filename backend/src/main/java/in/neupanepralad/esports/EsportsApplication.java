b0918537fc0fd7b91371bea4f44d4cdf300a1f4c
prakash69-code <neupaneprakash9999@gmail.com>
2026-07-16 10:46:18 +0545

    feat: initialized folder structure

---
+++ backend/src/main/java/in/neupanepralad/esports/EsportsApplication.java
@@ -0,0 +1,13 @@
+package in.neupanepralad.esports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsportsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/in/neupanepralad/esports/EsportsApplicationTests.java
@@ -0,0 +1,13 @@
+package in.neupanepralad.esports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
